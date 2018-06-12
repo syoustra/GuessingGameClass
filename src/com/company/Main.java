4269d014847288e374f3174b436bdd5d4d32a102
Stephanie Youstra <steph.you07@gmail.com>
2018-06-12 11:48:12 -0400

    Random Number practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+
+
+
+
+        // HOW TO GET A USABLE RANDOM NUMBER
+//        double randomNumber;
+//        randomNumber = Math.random();
+//        System.out.println("The Random Number is: " + randomNumber);
+//
+//        double randomNumberTimesHundred = randomNumber * 100;
+//        System.out.println("The Random Number times 100 is :" + randomNumberTimesHundred);
+//
+//        int randomNumberRoundedToCeil = (int) Math.ceil(randomNumberTimesHundred);
+//        System.out.println("The Rounded ciel is: " + randomNumberRoundedToCeil);
+//
+//        int randomNumberRoundedToFloor = (int) Math.floor(randomNumberTimesHundred);
+//        System.out.println("The Rounded to Floor is: " + randomNumberRoundedToFloor);
+//
+//        int randomNumberRoundedNormally = (int) Math.round(randomNumberTimesHundred);
+//        System.out.println("The Rounding that we're used to: " + randomNumberRoundedNormally);
+
+
+
     }
 }

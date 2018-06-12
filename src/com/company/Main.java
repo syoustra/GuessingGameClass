ea8c30909abb9ee3d66ebc54eaf9552d67658dd9
Stephanie Youstra <steph.you07@gmail.com>
2018-06-12 13:49:03 -0400

    Added user input and compared to random number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,22 +10,37 @@ Compare guess to the number
  */
 
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        //Initialize our scanner, so we can take input from the user
+        Scanner input = new Scanner(System.in);
+
         //Give Instructions
         System.out.println("Welcome to the Guessing Game! I'm thinking of a number between 1 and 100. Can you figure it out?");
 
         //Pick the Random Number
         int randomNumber = (int) Math.ceil(Math.random() * 100);
-        System.out.println(randomNumber);
+//        System.out.println(randomNumber);
 
+        //Get the Guess from the User
+        int guessedNumber;
+        System.out.println("Guess a number between 1 and 100");
+        guessedNumber = input.nextInt();
+        System.out.println("You guessed the number " + guessedNumber);
 
-
-
+        //Compare the Guess to the Random Number
+        if (randomNumber > guessedNumber) {
+            System.out.println("Your guess is too low. Guess higher.");
+        } else if (randomNumber < guessedNumber) {
+            System.out.println("Your guess is too high. Guess lower.");
+        } else {
+            System.out.println("Congratulations! You guessed my number " + randomNumber + "!!!");
+        }
 
 
 
